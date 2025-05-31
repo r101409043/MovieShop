35752c0987e0169c9d2c4578ae384c5b28dbb6cd
r101409043 <34722067+r101409043@users.noreply.github.com>
2025-05-31 15:48:04 -0400

    Add files via upload

---
+++ MovieShop.MVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop.MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IMovieService _movieService;
+
+    public HomeController(IMovieService movieService)
+    {
+        _movieService = movieService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var movies = await _movieService.GetTopRevenueMovies();
+        return View(movies);
+    }
+}
