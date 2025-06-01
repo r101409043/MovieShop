88dcafb5f09b20b39027d001e5b5f73262cadcc5
r101409043 <34722067+r101409043@users.noreply.github.com>
2025-05-31 23:17:49 -0400

    Delete ApplicationCore/Contracts directory

--- ApplicationCore/Contracts/Repository/IMovieRepository.cs
+++
@@ -1,8 +0,0 @@
-using ApplicationCore.Models;
-
-namespace ApplicationCore.Contracts.Repository;
-
-public interface IMovieRepository
-{
-    Task<IEnumerable<MovieCardModel>> GetTopRevenueMovies();
-}
--- ApplicationCore/Contracts/Services/IMovieService.cs
+++
@@ -1,8 +0,0 @@
-using ApplicationCore.Models;
-
-namespace ApplicationCore.Contracts.Services;
-
-public interface IMovieService
-{
-    Task<IEnumerable<MovieCardModel>> GetTopRevenueMovies();
-}
