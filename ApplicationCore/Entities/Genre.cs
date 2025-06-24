7b412d44295dacde3373718ac2f4507c2e31b086
Yu-Lung,Chen <r101409043@gmail.com>
2025-06-24 01:12:33 -0400

    add azure-pipelines.yml

--- ApplicationCore/Entities/Genre.cs
+++ ApplicationCore/Entities/Genre.cs
@@ -4,9 +4,9 @@ namespace ApplicationCore.Entities;
 
 public class Genre
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
     [Required]
-    public string Name { get; set; }
+    public string? Name { get; init; }
 
-    public ICollection<MovieGenre> MovieGenres { get; set; }
+    public ICollection<MovieGenre>? MovieGenres { get; init; }
 }
